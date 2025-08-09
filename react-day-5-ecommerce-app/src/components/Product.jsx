11b23adc214d9c88d4d22c1941b0260cf2c221d2
Ananthidevi <vananthi376@gmail.com>
2025-08-09 17:49:31 +0530

    react-5

--- react-day-5-ecommerce-app/src/components/Product.jsx
+++ react-day-5-ecommerce-app/src/components/Product.jsx
@@ -14,12 +14,15 @@ function Product() {
   return (
     <div className='p-4 md:p-8 bg-gray-100'>
         <h3 className='text-3xl md:text-3xl font-semibold text-center mb-6'>Our products</h3>
-    <div className='grid grid'>
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
+
         {
             Products.map(data => (
-                <div>
+                <div className='bg-white shadow rounded-md p-4 flex flex-col items-center'>
                     <img className='h-40 w-auto mb-4 object-contain' src = {data.image} alt =""/>
-                    <h4>${data.price}</h4>
+                    <h4 className='text-md md:text-lg font-semibold mb-3 text-center'>${data.title}</h4>
+                    <h5 className='text-red-500'>${data.price}</h5>
+                    <button className='bg-red-500 trxt-white px-6 py-4 rounded hover:bg-green-500'>Add to cart</button>
                     </div>
             ))
         }
